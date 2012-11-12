d2b8e127bf30bac0808e162a1712b4ecb535feb9
Daniel Berger <berger@cs.uni.kl.de>
2012-11-12 07:28:24 +0000

    Variable Delay Channel

---
+++ networks/VariableDelayChannel.cc
@@ -0,0 +1,36 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "VariableDelayChannel.h"
+
+VariableDelayChannel::VariableDelayChannel()
+    :cDatarateChannel()
+{
+    // TODO Auto-generated constructor stub
+}
+
+VariableDelayChannel::~VariableDelayChannel()
+{
+    // TODO Auto-generated destructor stub
+}
+
+
+void VariableDelayChannel::processMessage(cMessage *msg, simtime_t t, result_t& result)
+{
+    SimTime *addelay = new SimTime();
+    addelay->parse("1ms");
+    cDatarateChannel::setDelay(exponential((*addelay).dbl()));
+    cDatarateChannel::processMessage(msg, t, result);
+}
