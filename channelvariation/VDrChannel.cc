2591a90dfb3d6e651749ce123241c71dcc35b1bb
Daniel Berger <berger@cs.uni.kl.de>
2013-04-28 19:10:33 +0000

    weibull channel distribution

--- channelvariation/VDrChannel.cc
+++ channelvariation/VDrChannel.cc
@@ -4,8 +4,8 @@
 class VDrChannel : public cDatarateChannel
 {
   protected:
-    int meanDatarate;
-    int stdDatarate;
+    int scale;
+    int shape;
   public:
     explicit VDrChannel(const char *name = NULL);
     virtual ~VDrChannel();
@@ -26,17 +26,18 @@ VDrChannel::~VDrChannel()
 void VDrChannel::initialize()
 {
     cDatarateChannel::initialize();
-    meanDatarate = par("meanDatarate");
-    stdDatarate = par("stdDatarate");
+    scale = par("scale");
+    shape = par("shape");
     cDatarateChannel::setDatarate(
-            truncnormal(meanDatarate-0.0001,stdDatarate)+0.0001);
+            truncnormal(scale-0.0001,shape)+0.0001);
 }
 
 void VDrChannel::processMessage(cMessage *msg, simtime_t t, result_t& result)
 {
     cDatarateChannel::processMessage(msg, t, result);
-    if(stdDatarate != 0) {
-        double nextDatarate = truncnormal(meanDatarate-0.0001,stdDatarate)+0.0001; //in expectancy this will result in meanDr = mean, however, no 0 values will occur
+    if(shape != 0) {
+        //double nextDatarate = truncnormal(meanDatarate-0.0001,stdDatarate)+0.0001; //in expectancy this will result in meanDr = mean, however, no 0 values will occur
+        double nextDatarate = weibull(scale,shape); //weibull(scale,shape)
         cDatarateChannel::setDatarate(nextDatarate);
     }
 }
