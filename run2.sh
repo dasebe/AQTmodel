b238c150eee39e0eebb2f3155d2b850c07a077e0
Daniel Berger <berger@cs.uni.kl.de>
2013-05-03 20:49:00 +0000

    parallelization

--- run2.sh
+++
@@ -1,9 +0,0 @@
-nice -n 3 ionice -c2 -n7 ./adversarialQueueing -c cdBB -u Cmdenv -r 0..3149
-nice -n 3 ionice -c2 -n7 ./adversarialQueueing -c cdCE3 -u Cmdenv -r 0..1700
-nice -n 3 ionice -c2 -n7 ./adversarialQueueing -c cdCE3 -u Cmdenv -r 1701..3149
-nice -n 3 ionice -c2 -n7  ./adversarialQueueing -c cdCE71 -u Cmdenv -r 0..900
-nice -n 3 ionice -c2 -n7  ./adversarialQueueing -c cdCE71 -u Cmdenv -r 901..1700
-nice -n 3 ionice -c2 -n7  ./adversarialQueueing -c cdCE71 -u Cmdenv -r 1701..2400
-nice -n 3 ionice -c2 -n7  ./adversarialQueueing -c cdCE71 -u Cmdenv -r 2401..3149
-
-
